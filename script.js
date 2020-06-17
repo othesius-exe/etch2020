489d9a349d733d762f007a9124d1a86b28f8f050
Caleb Imler <calebqimler@gmail.com>
2020-06-17 14:10:34 -0400

    added id assignments to divs

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const container = document.getElementById('container');
 const gridSize = 16;
+let currentDivId = gridSize * gridSize;
 
 for (i = 0; i < gridSize; i++) {
     const newRow = document.createElement('div');
     container.appendChild(newRow);
-    newRow.classList.add(".row");
+    newRow.classList.add(".rowItem");
     for (j = 0; j < gridSize; j++) {
         const newDiv = document.createElement('div');
         newRow.appendChild(newDiv);
-        newDiv.classList.add('row');
+        newDiv.classList.add('rowItem');
+        newDiv.setAttribute("id", `${currentDivId}`);
+        currentDivId--;
+        console.log(currentDivId);
     }
-
 }
+
+window.addEventListener('mouseOver', function() { 
+    
+})

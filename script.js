a80e9357c0a788c8007f5768f384988e94278684
Caleb Imler <calebqimler@gmail.com>
2020-06-18 18:02:41 -0400

    creates new grid when cleared

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let currentDivId = gridSize * gridSize;
 for (i = 0; i < gridSize; i++) {
     const newRow = document.createElement('div');
     container.appendChild(newRow);
-    newRow.classList.add(".rowItem");
+    newRow.classList.add("rowContainer");
     for (j = 0; j < gridSize; j++) {
         const newDiv = document.createElement('div');
         newRow.appendChild(newDiv);
@@ -17,22 +17,62 @@ for (i = 0; i < gridSize; i++) {
 }
 
 const divToColor = document.querySelectorAll('div');
-const removeColor = document.querySelectorAll('div');
+const removeDiv = document.querySelectorAll('div');
 
 divToColor.forEach((div)  => {
     if(div.classList.contains('rowItem')) {
         div.addEventListener('mouseover', (e) =>
-        div.classList.toggle('etchedItem'));
+        div.classList.add('etchedItem'));
     }
 })
 
-const clearButton = document.querySelector('button');
-
+const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', (e) => {
-    removeColor.forEach((div) => {
-        if(div.classList.contains('etchedItem')) {
-            div.classList.remove('etchedItem');
-        }
-    })
-    gridSize = prompt('How big would you like your grid?', 16);
+    console.log(e);
+    clearGrid();
+    gridSize = prompt('Pick a new grid square', 16)
+
+    if (isNaN(gridSize) || gridSize == '') {
+        gridSize = 16;
+        createGrid(gridSize);
+    } else {
+        createGrid(gridSize);
+    }
 })
+
+function createGrid(square) {
+    const container = document.getElementById('container');
+    let gridSquare = square;
+    let currentDivId = square * square;
+
+    for (i = 0; i < gridSquare; i++) {
+        const newRow = document.createElement('div');
+        container.appendChild(newRow);
+        newRow.classList.add('rowContainer');
+        for (j = 0; j < gridSquare; j++) {
+            const newDiv = document.createElement('div');
+            newRow.appendChild(newDiv);
+            newDiv.classList.add('rowItem');
+            newDiv.setAttribute("id", `${currentDivId}`);
+            currentDivId--;
+            console.log(currentDivId);
+        }
+    }
+
+    const divToColor = document.querySelectorAll('div');
+    const removeDiv = document.querySelectorAll('div');
+
+    divToColor.forEach((div)  => {
+    if(div.classList.contains('rowItem')) {
+        div.addEventListener('mouseover', (e) =>
+        div.classList.add('etchedItem'));
+    }
+})
+}
+
+function clearGrid() {
+    const container = document.getElementById('container');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
